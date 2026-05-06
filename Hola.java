2437e146db9c1996d6acc1b16dc5541b144c3c72
AlexSney <elpibefachero2006@gmail.com>
2026-05-05 20:01:11 -0500

    esta es la segunda version

--- Hola.java
+++ Hola.java
@@ -1,5 +1,7 @@
+package GIT;
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola, mundo!");
+        System.out.println("Bienvenidos a Git");
     }
 }
