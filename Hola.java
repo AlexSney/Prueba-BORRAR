937f12ec4c5b8f9a991954981cbafd41a94f65c2
AlexSney <elpibefachero2006@gmail.com>
2026-05-05 20:44:39 -0500

    Update welcome message in Hola.java
    quiero mi mama quiero mi papa

--- Hola.java
+++ Hola.java
@@ -2,6 +2,6 @@ package GIT;
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola, mundo!");
-        System.out.println("Bienvenidos a Git");
+        System.out.println("Bienvenidos a quiero mi mmama quiere mi papa");
     }
 }
