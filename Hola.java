79bd2df13250e246f1f03b198cda08dc1dcd4474
AlexSney <elpibefachero2006@gmail.com>
2026-05-05 20:58:36 -0500

    esta es la ultima version

---
+++ Hola.java
@@ -0,0 +1,13 @@
+package GIT;
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo!");
+<<<<<<< HEAD
+        System.out.println("Bienvenidos a Git");
+        System.out.println("Este es un ejemplo de código en Java");
+        
+=======
+        System.out.println("Bienvenidos a quiero mi mmama quiere mi papa");
+>>>>>>> 937f12ec4c5b8f9a991954981cbafd41a94f65c2
+    }
+}
